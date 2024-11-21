30eeccacb03f7795e400261feace8cb4ebab13dd
Jay <patej13@farmingdale.edu>
2024-11-20 20:50:22 -0500

    ToDo2 and 3 done

--- app/src/main/java/edu/farmingdale/threadsexample/FibonacciDemo.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/FibonacciDemo.kt
@@ -22,10 +22,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.lang.reflect.Modifier
 import java.text.NumberFormat
 import java.util.Locale
 
@@ -33,39 +33,10 @@ import java.util.Locale
 fun FibonacciDemoNoBgThrd() {
     var answer by remember { mutableStateOf("") }
     var textInput by remember { mutableStateOf("40") }
-
-    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-        //Spacer(modifier = Modifier.padding(60.dp))
-        Row {
-            TextField(
-                value = textInput,
-                onValueChange = { textInput = it },
-                label = { Text("Number?") },
-                singleLine = true,
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number
-                )
-            )
-            Button(onClick = {
-                val num = textInput.toLongOrNull() ?: 0
-                val fibNumber = fibonacci(num)
-                answer = NumberFormat.getNumberInstance(Locale.US).format(fibNumber)
-            }) {
-                Text("Fibonacci")
-            }
-        }
-
-        Text("Result: $answer")
-    }
-}
-
-@Composable
-fun FibonacciDemoWithCoroutine(){
-    var answer by remember { mutableStateOf("") }
-    var textInput by remember { mutableStateOf("40") }
     val coroutineScope = rememberCoroutineScope()
 
     Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        Spacer(modifier = Modifier.padding(60.dp))
         Row {
             TextField(
                 value = textInput,
@@ -82,12 +53,10 @@ fun FibonacciDemoWithCoroutine(){
                     val fibNumber = fibonacci(num)
                     answer = NumberFormat.getNumberInstance(Locale.US).format(fibNumber)
                 }
-
             }) {
                 Text("Fibonacci")
             }
         }
-
         Text("Result: $answer")
     }
 }
